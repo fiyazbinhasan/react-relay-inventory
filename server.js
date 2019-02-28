1901469532754ad58d4504bb3ce37d0c52d0e646
Fiyaz Hasan <fiyazhasan@live.com>
2019-02-28 22:33:27 +0600

    babel template

---
+++ server.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+let app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_module/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
